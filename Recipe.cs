85dd68a4d31276d76debb372ea51a0692e92b44d
YounesAbady <102363875+YounesAbady@users.noreply.github.com>
2022-06-28 11:14:19 +0200

    Initial commit

---
+++ Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_1
+{
+    internal class Recipe
+    {
+        public Recipe(string title, List<string> ingredients, List<string> instructions, List<string> categories)
+        {
+            Title = title;
+            Ingredients = ingredients;
+            Instructions = instructions;
+            Categories = categories;
+        }
+
+        public string Title { get; set; }
+        public List<string> Ingredients { get; set; }
+        public List<string> Instructions { get; set; }
+        public List<string> Categories { get; set; }
+    }
+}
